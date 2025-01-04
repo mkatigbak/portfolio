0ae9c6161770dbbd8fe4982199683c250051ce1c
Mark Katigbak <mark.katigbak@outlook.com>
2025-01-04 03:19:55 -0700

    Removed javascript folder and file then added stack cards scroll animation.

--- javascript/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('scroll', function() {
-    const projects = document.querySelectorAll('.project');
-    const scrollPosition = window.scrollY;
-
-    projects.forEach((project, index) => {
-        const offset = index * 100; // Adjust this value to control the stacking effect
-        project.style.top = `${scrollPosition - offset}px`;
-    });
-});
