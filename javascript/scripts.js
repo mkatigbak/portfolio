8a1291d143b2126a81eed60d0f908aecbf585bef
Mark Katigbak <mark.katigbak@outlook.com>
2025-01-05 15:27:00 -0700

    Refactor fade-out animation: rename keyframes and update duration for smoother transition

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,7 +1,7 @@
 document.getElementById('back-to-top').addEventListener('click', function(event) {
     event.preventDefault();
-    document.body.classList.add('zoom-out-fade-out');
+    document.body.classList.add('fade-out');
     setTimeout(function() {
         window.location.href = 'https://mkatigbak.github.io/portfolio/';
-    }, 1500);
+    }, 1000);
 });
