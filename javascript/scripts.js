56b919d486c8819bfea0b70f57fbb52a1706ef2f
Mark Katigbak <mark.katigbak@outlook.com>
2025-01-06 14:35:17 -0700

    Remake to do a mobile first responsive website

--- javascript/scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('back-to-top').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.body.classList.add('fade-out');
-    setTimeout(function() {
-        window.location.href = 'https://mkatigbak.github.io/portfolio/';
-    }, 1000);
-});
